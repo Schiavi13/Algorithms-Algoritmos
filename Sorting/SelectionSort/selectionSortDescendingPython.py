7932349dac3066a3a4a1a4e0dd59f125489ba4c6
Luis Martínez <lufmartinezmu@gmail.com>
2018-06-07 21:44:56 -0500

    Create selectionSortDescendingPython.py

---
+++ Sorting/SelectionSort/selectionSortDescendingPython.py
@@ -0,0 +1,16 @@
+a_list = [5,2,4,6,1,3]
+
+def selectionSort(a_list):
+    for i in range(0,len(a_list)-1):
+        ind = i
+
+        for j in range(i,len(a_list)-1):
+            if a_list[j+1]>a_list[ind]:
+                ind = j+1
+        
+        smallest = a_list[ind]
+        a_list[ind] = a_list[i]
+        a_list[i] = smallest
+
+selectionSort(a_list)
+print(a_list)
