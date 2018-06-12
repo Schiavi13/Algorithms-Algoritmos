9a4c551f32e99ed1109e73cb3ad86c010622fdee
Luis Martínez <lufmartinezmu@gmail.com>
2018-06-12 17:12:23 -0500

    Create mergeSortC.cpp

---
+++ Sorting/MergeSort/mergeSortC.cpp
@@ -0,0 +1,62 @@
+#include <stdio.h>
+
+void merge(int *arr, int p, int q, int r){
+	int n1 = q-p+1;
+	int n2 = r-q;
+	int L[n1], R[n2];
+	
+	for(int i=0;i<n1;i++)
+		L[i] = arr[p+i];      //This is different from the pseudocode, since the first index is 0
+		
+	for(int j=0;j<n2;j++)
+		R[j] = arr[q+j+1];	  //fix this too
+	
+	int i = 0;
+	int j = 0;
+	int k = p;
+	
+	while(i<n1 && j<n2){
+		if(L[i] <= R[j]){
+			arr[k] = L[i];
+			i = i+1;
+		}
+		else{
+			arr[k] = R[j];
+			j = j+1;
+		}
+		
+		k = k+1;
+	}
+	
+	while(i<n1){
+		arr[k] = L[i];
+		i = i+1;
+		k = k+1;
+	}
+	
+	while(j<n2){
+		arr[k] = R[j];
+		j = j+1;
+		k = k+1;
+	}
+	
+}
+
+void mergeSort(int *arr, int p, int r){
+	if(p<r){
+		int q = (r+p)/2;
+		mergeSort(arr,p,q);
+		mergeSort(arr,q+1,r);
+		merge(arr,p,q,r);
+	}	
+}
+
+int main()
+{
+	int arr[6]={5,2,4,6,1,3}, length=6;
+	
+	mergeSort(arr,0,length-1);
+	for(int i=0;i<6;i++)
+		printf("%d\n",arr[i]);
+	return 0;
+}
